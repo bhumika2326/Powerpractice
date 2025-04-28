7fa0d8f181948fc4c96662160f23a8aa8a4c31fd
Bhumika Ranwani <bhumikavranwani.bscibm2024@iar.ac.in>
2025-04-28 18:40:06 +0530

    This is my code for powerpractice

---
+++ Powerpracticecode.py
@@ -0,0 +1,10 @@
+#powerpractice is going on...
+a = 20
+b = 30
+c = 45
+if (a>b and a>c):
+    print("a is bigger number")
+elif(b>a and b>c):
+    print("b is bigger number")
+else:
+    print("C is bigger number")
